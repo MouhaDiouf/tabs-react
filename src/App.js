0e05bbf14ddc01d90d098b4c832de407505ce1d8
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-26 11:37:37 +0000

    Show loading or the jobs depending on the fetch state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 const url = 'https://course-api.netlify.app/api/react-tabs-project';
 
@@ -6,7 +6,24 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [value, setValue] = useState(0);
-  return <div className="App"></div>;
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  return <h2>Jobs</h2>;
 }
 
 export default App;
